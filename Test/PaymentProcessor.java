6f760c362991e9fb8cf60bde7a83ddcda52a0ea4
AyoubZekri <ayobzekri670@email.com>
2026-05-06 09:59:20 +0100

    PaymentProcessor    After

--- Test/PaymentProcessor.java
+++ Test/PaymentProcessor.java
@@ -1,9 +1,27 @@
+interface PaymentStrategy {
+    void pay(double amount);
+}
+
+class CardPayment implements PaymentStrategy {
+    public void pay(double amount) {
+        System.out.println("Card: " + amount);
+    }
+}
+
+class PaypalPayment implements PaymentStrategy {
+    public void pay(double amount) {
+        System.out.println("PayPal: " + amount);
+    }
+}
+
 class PaymentProcessor {
-    public void processPayment(String method, double amount) {
-        if (method.equals("card")) {
-            System.out.println("Card: " + amount);
-        } else if (method.equals("paypal")) {
-            System.out.println("PayPal: " + amount);
-        }
+    private PaymentStrategy strategy;
+
+    public void setStrategy(PaymentStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void process(double amount) {
+        strategy.pay(amount);
     }
 }

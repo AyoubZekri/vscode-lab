9adee66d893b7be716bfa2c62f92a8d1c08b424a
AyoubZekri <ayobzekri670@email.com>
2026-05-06 10:12:25 +0100

    OrderCalculator After

--- Test/OrderCalculator.java
+++ Test/OrderCalculator.java
@@ -1,4 +1,4 @@
-class OrderCalculator {
+class CalculationService {
     public double calculateTotal(double subtotal, double taxRate) {
         double tax = subtotal * taxRate;
         return subtotal + tax;
@@ -9,13 +9,31 @@ class OrderCalculator {
     }
 }
 
-class DiscountCalculator {
-    public double calculateTotal(double subtotal, double taxRate) {
-        double tax = subtotal * taxRate;
-        return subtotal + tax;
+class OrderCalculator {
+    private final CalculationService calculationService;
+
+    public OrderCalculator(CalculationService calculationService) {
+        this.calculationService = calculationService;
     }
 
-    public double applyDiscount(double total, double discountRate) {
-        return total * (1 - discountRate);
+    public double calculateOrderTotal(double subtotal, double taxRate) {
+        return calculationService.calculateTotal(subtotal, taxRate);
+    }
+
+    public double applyOrderDiscount(double total, double discountRate) {
+        return calculationService.applyDiscount(total, discountRate);
+    }
+}
+
+class DiscountCalculator {
+    private final CalculationService calculationService;
+
+    public DiscountCalculator(CalculationService calculationService) {
+        this.calculationService = calculationService;
+    }
+
+    public double calculateDiscountedTotal(double subtotal, double taxRate, double discountRate) {
+        double total = calculationService.calculateTotal(subtotal, taxRate);
+        return calculationService.applyDiscount(total, discountRate);
     }
 }
